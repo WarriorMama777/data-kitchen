98d8053d90be0cfbb472caacac0fedb81d00c35c
WarriorMama777 <N7G0Ogwa91f5ZQcU>
2024-03-29 05:58:15 +0900

    指定した文字以降を削除などを追加

--- renamer.py
+++ renamer.py
@@ -36,6 +36,32 @@ def rename_files(args):
             old, new = args.replace.split("->")
             new_name = new_name.replace(old, new)
 
+        # 指定した文字以降を削除
+        if args.del_after:
+            pos = new_name.find(args.del_after)
+            if pos != -1:
+                new_name = new_name[:pos + len(args.del_after)]
+
+        # 指定した文字以前を削除
+        if args.del_before:
+            pos = new_name.find(args.del_before)
+            if pos != -1:
+                new_name = new_name[pos:]
+
+        # 指定した文字の後ろに文字を追加
+        if args.add_after:
+            search_str, add_str = args.add_after.split(",")
+            pos = new_name.find(search_str)
+            if pos != -1:
+                new_name = new_name[:pos + len(search_str)] + add_str + new_name[pos + len(search_str):]
+
+        # 指定した文字の前に文字を追加
+        if args.add_before:
+            search_str, add_str = args.add_before.split(",")
+            pos = new_name.find(search_str)
+            if pos != -1:
+                new_name = new_name[:pos] + add_str + new_name[pos:]
+
         if new_name != filename:
             old_path = os.path.join(directory, filename)
             new_path = os.path.join(directory, new_name)
@@ -56,6 +82,11 @@ if __name__ == "__main__":
     parser.add_argument("--add_number_first", action='store_true', help="先頭に連番を追加")
     parser.add_argument("--add_number_last", action='store_true', help="末尾に連番を追加")
     parser.add_argument("--replace", type=str, help="文字を置換する(例: 'neko->piyo')")
+    parser.add_argument("--del_after", type=str, help="指定した文字以降を削除")
+    parser.add_argument("--del_before", type=str, help="指定した文字以前を削除")
+    parser.add_argument("--add_after", type=str, help="指定した文字の後ろに文字を追加 (形式: '検索文字列,追加文字列')")
+    parser.add_argument("--add_before", type=str, help="指定した文字の前に文字を追加 (形式: '検索文字列,追加文字列')")
+
 
     args = parser.parse_args()
 
