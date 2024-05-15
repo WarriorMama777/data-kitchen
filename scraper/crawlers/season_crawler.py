362affaa4d92e4557cfb129f9d8d41ef75a1bf44
WarriorMama777 <N7G0Ogwa91f5ZQcU>
2024-05-16 04:35:34 +0900

    シーズンクローラを元に戻す

--- scraper/crawlers/season_crawler.py
+++ scraper/crawlers/season_crawler.py
@@ -1,122 +1,64 @@
-import requests
 from bs4 import BeautifulSoup
 from scraper.crawlers import episode_crawler
 import re
+import urllib.request
+import urllib.error
 from scraper.utils.colors import Colors
-import time
-from tqdm import tqdm
 
 class SeasonCrawler:
-    def __init__(self, url):
-        self.url = url
-        self.name = None
-        self.current_url = None
-        self.page = 1
-        self.max_retries = 3
+    url = None
+    name = None
 
-    def crawl(self):
+    def crawl(self, url, download_func):
         ep_links = []
-        self.current_url = self.url
-        with tqdm(total=self.max_retries) as pbar:
-            while self.current_url:
-                retries = 0
-                while retries < self.max_retries:
-                    try:
-                        headers = {
-                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; rv:124.0) Gecko/20100101 Firefox/124.0'
-                        }
-                        response = requests.get(self.current_url, headers=headers)
-                        response.raise_for_status()
-                        break
-                    except requests.exceptions.RequestException as e:
-                        Colors.print(f"Error opening URL: {e}", Colors.RED)
-                        retries += 1
-                        time.sleep(2 ** retries)
-                        if retries >= self.max_retries:
-                            Colors.print(f"Max retries reached. Skipping {self.current_url}", Colors.YELLOW)
-                            self.current_url = None
-                            break
-                    finally:
-                        pbar.update(1)
-
-                content = response.content
-                beautiful_soup = self.parse_content(content)
-                ep_links.extend(self.extract_episode_links(beautiful_soup))
-
-                # 次のページへのリンクを確認する
-                self.current_url = self.get_next_page_url(beautiful_soup)
-                self.page += 1
-
-                # エピソード リンクをクロールしてダウンロードする
-                if ep_links:
-                    self.download_episode(ep_links.pop(0))
-
-            Colors.print(f"{self.url} crawling and downloading finished.", Colors.YELLOW)
-
-    def download_episode(self, ep_link):
-        crawler = episode_crawler.EpisodeCrawler()
-        episode_result = crawler.crawl(ep_link)
-        pic_links = episode_result['links']
-
-        # ダウンローダークラスをインポート
-        from scraper.downloader import Downloader
-
-        downloader = Downloader()
-        downloader.downloadUrls("path/to/output/directory", pic_links)
-        
-    def parse_content(self, content):
-        try:
-            return BeautifulSoup(content, 'html.parser')
-        except Exception as e:
-            Colors.print(f"Error occurred while parsing the page: {e}", Colors.RED)
-            return None
-
-    def extract_episode_links(self, beautiful_soup):
-        ep_links = []
-        for dom_link in beautiful_soup.find_all('a', class_='btn', href=re.compile("^.*?/episodeimages.php\?")):
-            href = dom_link.get('href')
-            if href:
-                href = re.sub("^https?://", "https://", href)  # URL の正規化
-                match = re.search(r"https://fancaps.net/.*?/episodeimages.php\?\d+-(.*?)/", href)
-                if match:
-                    if not self.name:
-                        self.name = match.group(1)
-                    if self.name == match.group(1):
-                        ep_links.append(href)
-        return ep_links
-
-    def get_next_page_url(self, beautiful_soup):
-        next_link = beautiful_soup.find("a", text=re.compile(r'Next', re.IGNORECASE), href=lambda href: href and href != "#")
-        if next_link:
-            return f"{self.url}&page={self.page}"
-        else:
-            return None
-
-    def crawl_episode_links(self, ep_links):
         pic_links = []
-        crawler = episode_crawler.EpisodeCrawler()
-        with tqdm(total=len(ep_links)) as pbar:
+        self.url = url
+        current_url = self.url
+        page = 1
+
+        while current_url:
+            try:
+                request = urllib.request.Request(current_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'})
+                content = urllib.request.urlopen(request)
+            except urllib.error.URLError as e:
+                Colors.print(f"Error occurred while opening the URL: {e.reason}", Colors.RED)
+                break
+            except urllib.error.HTTPError as e:
+                Colors.print(f"HTTP Error: {e.code} {e.reason}", Colors.RED)
+                break
+
+            try:
+                beautiful_soup = BeautifulSoup(content, 'html.parser')
+            except Exception as e:
+                Colors.print(f"Error occurred while parsing the page: {e}", Colors.RED)
+                break
+
+            for dom_link in beautiful_soup.find_all('a', class_='btn', href=re.compile("^.*?/episodeimages.php\?")):
+                href = dom_link.get('href')
+                if href:
+                    if not re.match("^https://.*?/episodeimages.php\?", href):
+                        href = 'https://fancaps.net' + dom_link.get('href')
+
+                    match = re.search(r"https://fancaps.net/.*?/episodeimages.php\?\d+-(.*?)/", href)
+                    if match:
+                        if not self.name:
+                            self.name = match.group(1)
+                        if self.name == match.group(1):
+                            ep_links.append(href)
+
+            if beautiful_soup.find("a", text=re.compile(r'Next', re.IGNORECASE), href=lambda href: href and href != "#"):
+                page += 1
+                current_url = url + f"&page={page}"
+            else:
+                current_url = None
+
             for ep_link in ep_links:
-                retries = 0
-                while retries < self.max_retries:
-                    try:
-                        episode_result = crawler.crawl(ep_link)
-                        pic_links.append(episode_result)
-                        Colors.print(f"\t{ep_link} crawled", Colors.GREEN)
-                        break
-                    except Exception as e:
-                        Colors.print(f"Failed to crawl {ep_link}. Retrying... ({retries+1}/{self.max_retries})", Colors.YELLOW)
-                        retries += 1
-                        time.sleep(2 ** retries)  # 指数バックオフでリトライ間隔を設定
-                        if retries >= self.max_retries:
-                            Colors.print(f"Max retries reached. Skipping {ep_link}", Colors.RED)
-                    finally:
-                        pbar.update(1)  # 進行状況バーを更新
+                try:
+                    episode_result = episode_crawler.EpisodeCrawler().crawl(ep_link)
+                    pic_links.append(episode_result)
+                    Colors.print(f"\t{ep_link} crawled", Colors.GREEN)
+                    download_func(episode_result)  # Call the download function for each episode
+                except Exception as e:
+                    Colors.print(f"Failed to crawl {ep_link}: {e}", Colors.RED)
 
         return pic_links
-
-# 使用例
-url = "https://fancaps.net/anime/showimages.php?22442-Zombie_Land_Saga_Revenge"
-crawler = SeasonCrawler(url=url)
-pic_links = crawler.crawl()
-print(pic_links)
