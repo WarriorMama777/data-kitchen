44b4f71f5b026d9b00e05a852f150d834d80ad15
WarriorMama777 <N7G0Ogwa91f5ZQcU>
2024-05-16 07:25:32 +0900

    保存席ディレクトリがない場合のエラー対処

--- my_scripts/fancaps_episode_url.py
+++ my_scripts/fancaps_episode_url.py
@@ -11,15 +11,11 @@ def fetch_episode_urls(anime_url, headers, verbose):
     while True:
         try:
             response = session.get(f"{anime_url}&page={page}", headers=headers)
-            if response.status_code != 200:
-                if verbose:
-                    print(f"ページ{page}の取得に失敗しました。ステータスコード: {response.status_code}")
-                break
+            response.raise_for_status()  # ステータスコードが200系でない場合にエラーを発生させる
         except requests.exceptions.RequestException as e:
             if verbose:
-                print(f"リクエスト中にエラーが発生しました: {e}")
+                print(f"URL: {anime_url} | Error: {e}")
             break
-
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a', href=True)
         found_new_link = False
@@ -39,12 +35,16 @@ def main():
     parser = argparse.ArgumentParser(description='Fetch Fancaps episode URLs.')
     parser.add_argument('--dir', type=str, help='Path to the input URL list file.', required=True)
     parser.add_argument('--save_dir', type=str, help='Directory to save the episode URLs file.', required=True)
-    parser.add_argument('--verbose', action='store_true', help='Print detailed process information.')
+    parser.add_argument('--verbose', action='store_true', help='Increase output verbosity')
     args = parser.parse_args()
 
+    # ユーザーエージェントを設定
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)  # 指定された保存ディレクトリが存在しない場合、作成する
 
     with open(args.dir, 'r') as file:
         anime_urls = [line.strip() for line in file.readlines()]
@@ -59,7 +59,8 @@ def main():
         for url in all_episode_urls:
             file.write(f"{url}\n")
 
-    print(f"Episode URLs saved to {output_path}")
+    if args.verbose:
+        print(f"Episode URLs saved to {output_path}")
 
 if __name__ == "__main__":
     main()
