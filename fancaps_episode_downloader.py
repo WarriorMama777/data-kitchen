33b59e04bca50c0eb9f18cc64644a6b18a2cf940
WarriorMama777 <N7G0Ogwa91f5ZQcU>
2024-05-17 10:36:06 +0900

    MovieとSeasonの統合

--- fancaps_episode_downloader.py
+++ fancaps_episode_downloader.py
@@ -71,6 +71,104 @@ class EpisodeCrawler:
             'links': pic_links
         }
 
+# SeasonCrawler クラスの定義
+class SeasonCrawler:
+    def __init__(self):
+        self.name = None
+
+    def crawl(self, url):
+        epLinks = []
+        picLinks = []
+        currentUrl = url
+        page = 1
+
+        while currentUrl:
+            try:
+                response = requests.get(currentUrl, headers={'User-Agent': 'Mozilla/5.0'})
+                response.raise_for_status()
+                content = response.content
+            except requests.exceptions.RequestException as e:
+                print(f"Error occurred while opening the URL: {e}", 'red')
+                break
+
+            beautifulSoup = BeautifulSoup(content, 'html.parser')
+
+            for DOMLink in beautifulSoup.find_all('a', class_='btn', href=re.compile("^.*?/episodeimages.php\?")):
+                href = DOMLink.get('href')
+                if href:
+                    if not re.match("^https://.*?/episodeimages.php\?", href):
+                        href = 'https://fancaps.net' + href
+
+                    match = re.search(r"https://fancaps.net/.*?/episodeimages.php\?\d+-(.*?)/", href)
+                    if match:
+                        if not self.name:
+                            self.name = match.group(1)
+                        if self.name == match.group(1):
+                            epLinks.append(href)
+            if beautifulSoup.find("a", text=re.compile(r'Next', re.IGNORECASE), href=lambda href: href and href != "#"):
+                page += 1
+                currentUrl = url + f"&page={page}"
+            else:
+                currentUrl = None
+
+        episodeCrawler = EpisodeCrawler()
+        for epLink in epLinks:
+            episodeResult = episodeCrawler.crawl(epLink)
+            picLinks.extend(episodeResult)  # epLinkから取得した画像リンクを追加
+
+        return {"subfolder": self.name, "links": picLinks}
+
+# MovieCrawler クラスの定義
+class MovieCrawler:
+    def crawl(self, url):
+        picLinks = []
+        currentUrl = url
+        pageNumber = 1
+        alt = None
+
+        try:
+            match = re.search(r"https://fancaps.net\/.*\?name=(.*)&", url)
+            if match:
+                subfolder = match.group(1)
+            else:
+                raise ValueError("URL does not contain a valid subfolder name.")
+        except ValueError as e:
+            print(f"Error extracting subfolder: {e}")
+            return
+
+        while currentUrl:
+            try:
+                response = requests.get(currentUrl, headers={'User-Agent': 'Mozilla/5.0'})
+                beautifulSoup = BeautifulSoup(response.text, "html.parser")
+            except Exception as e:
+                print(f"Error fetching or parsing page: {e}")
+                break
+
+            for img in beautifulSoup.find_all("img", src=re.compile("^https://moviethumbs.fancaps.net/")):
+                imgSrc = img.get("src")
+                imgAlt = img.get("alt")
+                if not alt:
+                    alt = imgAlt
+                if alt == imgAlt:
+                    picLinks.append(imgSrc.replace("https://moviethumbs.fancaps.net/", "https://mvcdn.fancaps.net/"))
+
+            try:
+                next = url.replace(f'https://fancaps.net/movies/','') +f"&page={pageNumber + 1}"
+                nextPage = beautifulSoup.find("a", href=next)
+                if nextPage:
+                    pageNumber += 1
+                    currentUrl = url + f"&page={pageNumber}"
+                else:
+                    currentUrl = None
+            except Exception as e:
+                print(f"Error processing next page: {e}")
+                break
+
+        return {
+            'subfolder': subfolder,
+            'links': picLinks
+        }
+
 # クローラークラスの定義
 class Crawler:
     def crawl(self, url):
@@ -78,17 +176,18 @@ class Crawler:
 
         url_support = UrlSupport()
         url_type = url_support.getType(url)
+        # Crawler クラス内の変更点
         if url_type == 'season':
             print(f"\t\"{url_type}\" url detected")
-            crawler = EpisodeCrawler()
-            output = crawler.crawl(url)
+            crawler = SeasonCrawler()  # SeasonCrawlerのインスタンス化
+            output = [crawler.crawl(url)]  # SeasonCrawlerのcrawlメソッドの戻り値はリスト内に格納
         elif url_type == 'episode':
             print(f"\t\"{url_type}\" url detected")
             crawler = EpisodeCrawler()
             output = [crawler.crawl(url)]
         elif url_type == 'movie':
             print(f"\t\"{url_type}\" url detected")
-            crawler = EpisodeCrawler()
+            crawler = MovieCrawler()
             output = [crawler.crawl(url)]
         else:
             return []
